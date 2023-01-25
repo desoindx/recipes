ba37ec80d35561ad25ddd4dd41ee98bb98447777
Desoindx <xavier.desoindre@hotmail.fr>
2023-01-25 17:11:29 +0100

    feat(matomo): Remove matomo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,19 +7,6 @@ function MyApp({ Component, pageProps }) {
     <>
     <Head>
       <title>Weekly recipes</title>
-      <script type="text/javascript">
-        {`var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="https://weeklyrecipes.matomo.cloud/";
-          _paq.push(['setTrackerUrl', u+'matomo.php']);
-          _paq.push(['setSiteId', '1']);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.async=true; g.src='//cdn.matomo.cloud/weeklyrecipes.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-        })();`}
-      </script>
     </Head>
       <Header />
       <Component {...pageProps} />
