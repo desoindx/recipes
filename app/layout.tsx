0ebf20ed470382205d2e62ae1f0ec1af1459df90
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-01-20 14:06:13 +0100

    chore: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next'
 import { Header } from 'components/Header'
 import '../styles/globals.css'
@@ -11,6 +12,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="fr">
     <body>
       <Header />
+      <Analytics />
       <main>{children}</main>
     </body>
   </html>
