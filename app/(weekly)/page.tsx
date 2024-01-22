134598e6748ed70e2a6e65c4ab3ba40d48143d5b
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-01-22 10:59:17 +0100

    fix: Re fetch data every day

--- app/(weekly)/page.tsx
+++ app/(weekly)/page.tsx
@@ -1,6 +1,8 @@
 import { getRecipes } from 'services/recipes'
 import SelectableRecipes from 'components/Recipe/SelectableRecipes'
 
+export const revalidate = 3600 * 24
+
 const Home = async () => {
   const result = await getRecipes()
 
