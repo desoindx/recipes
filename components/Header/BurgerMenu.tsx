e2c6b9d124024811f2690f2f6c3f4aeef8376f7b
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-03-11 11:18:07 +0100

    chore: useless fragment

--- components/Header/BurgerMenu.tsx
+++ components/Header/BurgerMenu.tsx
@@ -4,20 +4,19 @@ import BurgerMenuCheckBox from './BurgerMenuCheckBox'
 import menu from './menu.config'
 
 const BurgerMenu = () => (
-  <>
-    <div className={styles.container}>
-      <span>Weekly recipes</span>
-      <BurgerMenuCheckBox />
-      <ul className={styles.menu}>
-        {menu.map((item) => (
-          <li key={item.href}>
-            <Link href={item.href} prefetch={false}>
-              {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
+  <div className={styles.container}>
+    <span>Weekly recipes</span>
+    <BurgerMenuCheckBox />
+    <ul className={styles.menu}>
+      {menu.map((item) => (
+        <li key={item.href}>
+          <Link href={item.href} prefetch={false}>
+            {item.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
 )
+
 export default BurgerMenu
