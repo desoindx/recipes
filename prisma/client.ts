f65bf4a4778ca29983358bc22db5faca84597030
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-04-27 14:22:58 +0200

    chore: proper prisma upgrade

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,5 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from './src/prisma'
 
-const prisma = new PrismaClient()
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+const globalForPrisma = global as unknown as {
+  prismaClient: PrismaClient | undefined
+}
 
-export default prisma
+export const prismaClient = globalForPrisma.prismaClient ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prismaClient = prismaClient
+}
